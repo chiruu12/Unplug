dc316c1336e9b7c9563a792c153ce6a68a70a7c7
chiruu12 <chirag.gupta.290403@gmail.com>
2026-05-21 05:46:13 +0530

    export FirecrawlProvider from content package

--- sdk/src/unplug/providers/content/__init__.py
+++ sdk/src/unplug/providers/content/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from unplug.providers.content.firecrawl import FirecrawlProvider
 from unplug.providers.content.protocol import CleanResult, ContentProvider, ScrapedContent
 
-__all__ = ["CleanResult", "ContentProvider", "ScrapedContent"]
+__all__ = ["CleanResult", "ContentProvider", "FirecrawlProvider", "ScrapedContent"]
